d08c2dcb60e0329bbe93ab008c5c7d6938bc291e
Hemant Bhanoo <github@hbhanoo.fastmail.fm>
2016-01-17 22:46:52 -0800

    add a bit of a documentation

--- feedgen.rb
+++ feedgen.rb
@@ -1,3 +1,18 @@
+#
+# Generate an RSS feed using a YML file.
+# The idea is to make the YML file as DRY as possible
+# so you can do interpolation using %{name} in any value.
+# - Variables are defined in a special "vars" section, in any
+# level of heirarchy.
+# - A special "defaults" key exists at the top level, which
+#   lets you specify default key/value pairs for any 1st level key (kludge)
+#
+# There's some additional massaging that happens in the massage_params function :)
+#
+# Finally, I wanted to be able to generate pretty much the same feed twice:
+# - with a video enclosure
+# - with an audio enclosre
+# so there's a little bit of support for that at the end.
 require 'builder'
 require 'yaml'
 require 'net/http'
